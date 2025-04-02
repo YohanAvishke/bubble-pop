486baec720a2694876980c7cb9371ea1f09275a4
YohanAvishke <yaediriweera@gmail.com>
2025-04-02 15:37:35 +1100

    Initial Commit

---
+++ BubblePop/BubblePopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopApp.swift
+//  BubblePop
+//
+//  Created by Yohan Ediriweera on 2025-04-02.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
